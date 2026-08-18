8f04c2650410fb3a2c95dbe6e7876372ed9c2da6
ckchng <chngcheekheng@gmail.com>
2026-08-18 11:53:06 -0700

    Update Multi-layer backpropagation (Build a Neural Net 3/5)

--- foundations/multi_layer_backprop.py
+++ foundations/multi_layer_backprop.py
@@ -6,9 +6,8 @@ class Solution:
         weight: (out_features, in_features)
         bias:   (out_features,)
         """
-        output = x @ weight.T + bias
-        cache = (x, weight)
-        return output, cache
+        cache = x, weight
+        return np.round(x @ weight.T + bias, 4), cache
 
 
     def linear_backward(self, doutput, cache):
@@ -16,17 +15,14 @@ class Solution:
         doutput: (batch, out_features)
         """
         x, weight = cache
-
-        dx = doutput @ weight
-        dweight = doutput.T @ x
-        dbias = np.sum(doutput, axis=0)
-
-        return dx, dweight, dbias
+        dl_dw = doutput.T @ x
+        dl_dx = doutput @ weight
+        dl_db = np.sum(doutput, axis=0)
+        return np.round(dl_dw, 4), np.round(dl_dx, 4), np.round(dl_db, 4)
 
 
     def relu_forward(self, x):
-        output = np.maximum(0, x)
-        return output, x
+        return np.maximum(0, x)
 
 
     def relu_backward(self, doutput, x):
@@ -34,15 +30,15 @@ class Solution:
 
 
     def mse_forward_backward(self, prediction, target):
-        difference = prediction - target
+        difference = prediction -  target
 
         loss = np.mean(difference**2)
-        dprediction = 2 * difference / difference.size
-
-        return loss, dprediction
+        dl_dpred = (2 * difference) / difference.size
+        return np.round(loss, 4), np.round(dl_dpred, 4)
     
 
     def forward_and_backward(self, x, W1, b1, W2, b2, y_true):
+        # make them array
         x = np.asarray(x, dtype=float)
         W1 = np.asarray(W1, dtype=float)
         b1 = np.asarray(b1, dtype=float)
@@ -50,29 +46,31 @@ class Solution:
         b2 = np.asarray(b2, dtype=float)
         y_true = np.asarray(y_true, dtype=float)
 
-        # Add a batch dimension for a single example.
         if x.ndim == 1:
             x = x[None, :]
 
         if y_true.ndim == 1:
             y_true = y_true[None, :]
 
-        # Forward pass
-        z1, linear1_cache = self.linear_forward(x, W1, b1)
-        a1, relu_cache = self.relu_forward(z1)
-        y_hat, linear2_cache = self.linear_forward(a1, W2, b2)
 
-        loss, dy_hat = self.mse_forward_backward(y_hat, y_true)
 
-        # Backward pass
-        da1, dW2, db2 = self.linear_backward(dy_hat, linear2_cache)
-        dz1 = self.relu_backward(da1, relu_cache)
-        _, dW1, db1 = self.linear_backward(dz1, linear1_cache)
+        # forward pass two times
+        z1, layer1_cache = self.linear_forward(x, W1, b1)
+        a1 = self.relu_forward(z1)
 
-        return {
-            "loss": round(float(loss), 4),
-            "dW1": np.round(dW1, 4).tolist(),
-            "db1": np.round(db1, 4).tolist(),
-            "dW2": np.round(dW2, 4).tolist(),
-            "db2": np.round(db2, 4).tolist(),
-        }
+        y_hat, layer2_cache = self.linear_forward(a1, W2, b2)
+
+        # mse loss
+        loss, dl_dy = self.mse_forward_backward(y_hat, y_true)
+
+        # compute backward
+        dl_dW2, dl_da1, dl_db2  = self.linear_backward(dl_dy, layer2_cache)
+        dl_dz1 = self.relu_backward(dl_da1, z1)
+
+        dl_dW1, _, dl_db1 = self.linear_backward(dl_dz1, layer1_cache)
+
+        return {'loss': loss,
+                'dW1': dl_dW1,
+                'db1': dl_db1,
+                'dW2': dl_dW2,
+                'db2': dl_db2}
